a5daa110d094a43dd2f918191d078d6d4a9201a4
gabrielviticov <gabrielviticov.ctt@gmail.com>
2022-11-08 21:24:22 -0300

    inclusao das classes pojo

--- app/src/main/java/com/unicid/alunoscheckin/model/Alunos.java
+++ app/src/main/java/com/unicid/alunoscheckin/model/Alunos.java
@@ -1,4 +1,87 @@
 package com.unicid.alunoscheckin.model;
 
 public class Alunos {
+    private Integer id;
+    private String registroAluno;
+    private String nomeCompleto;
+    private Integer cpf;
+    private String sexo;
+    private String dtaNascimento;
+    private String email;
+    private Integer telefone;
+    private String senha;
+
+    public Alunos(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRegistroAluno() {
+        return registroAluno;
+    }
+
+    public void setRegistroAluno(String registroAluno) {
+        this.registroAluno = registroAluno;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDtaNascimento() {
+        return dtaNascimento;
+    }
+
+    public void setDtaNascimento(String dtaNascimento) {
+        this.dtaNascimento = dtaNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
--- app/src/main/java/com/unicid/alunoscheckin/model/Cursa.java
+++ app/src/main/java/com/unicid/alunoscheckin/model/Cursa.java
@@ -1,4 +1,51 @@
 package com.unicid.alunoscheckin.model;
 
 public class Cursa {
+    private Integer id;
+    private String graduacao;
+    private String semestreTurma;
+    private Alunos idAlunos;
+    private Disciplinas idDisciplinas;
+
+    public Cursa(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGraduacao() {
+        return graduacao;
+    }
+
+    public void setGraduacao(String graduacao) {
+        this.graduacao = graduacao;
+    }
+
+    public String getSemestreTurma() {
+        return semestreTurma;
+    }
+
+    public void setSemestreTurma(String semestreTurma) {
+        this.semestreTurma = semestreTurma;
+    }
+
+    public Alunos getIdAlunos() {
+        return idAlunos;
+    }
+
+    public void setIdAlunos(Alunos idAlunos) {
+        this.idAlunos = idAlunos;
+    }
+
+    public Disciplinas getIdDisciplinas() {
+        return idDisciplinas;
+    }
+
+    public void setIdDisciplinas(Disciplinas idDisciplinas) {
+        this.idDisciplinas = idDisciplinas;
+    }
 }
